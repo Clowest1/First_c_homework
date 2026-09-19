6a72547a63873ec390a31130ac4a544029c6b887
Clowest1 <andrey2407228@gmail.com>
2026-09-19 21:13:03 +0300

    fixed comparison with zero

--- main.c
+++ main.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
-
+int cmp_double(double a, double b){
+    const double eps = 1e-9;
+    return (a - b > eps) ? 1 : ((b - a > eps) ? -1 : 0);
+}
 int main(){
     char func;
     double num1, num2;
     printf("Enter the operation (+, -, *, /): ");
-    if (scanf("%c", &func)!=1){
-        printf("Error: Invalid operation.\n");
+    if (scanf(" %c", &func)!=1){
+        printf("Error: Invalid input.\n");
         return 1;
     }
     if (func != '+' && func != '-' && func != '*' && func != '/'){
         printf("Error: Invalid operation.\n");
         return 1;
     }
-    printf("Enter two numbers: example: 2.5 3.5\n");
+    printf("Enter two numbers(example: 2.5 3.5):");
     if (scanf("%lf %lf", &num1, &num2) != 2) {
         printf("Error: Invalid numbers.\n");
         return 1;
@@ -24,7 +27,7 @@ int main(){
     } else if (func == '*'){
         printf("Result: %f\n", num1 * num2);
     } else if (func == '/'){
-        if (num2 != 0){
+        if (cmp_double(num2, 0.0) != 0) {
             printf("Result: %f\n", num1 / num2);
         } else {
             printf("Error: Division by zero is not allowed.\n");
