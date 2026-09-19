16be63beadafc8d4dbf2acc236cefdc93d4abbc7
Clowest1 <andrey2407228@gmail.com>
2026-09-19 20:46:33 +0300

    some more fixes

--- main.c
+++ main.c
@@ -4,12 +4,15 @@ int main(){
     char func;
     double num1, num2;
     printf("Enter the operation (+, -, *, /): ");
-    scanf(" %c", &func);
+    if (scanf("%c", &func)!=1){
+        printf("Error: Invalid operation.\n");
+        return 1;
+    }
     if (func != '+' && func != '-' && func != '*' && func != '/'){
         printf("Error: Invalid operation.\n");
         return 1;
     }
-    printf("Enter two numbers: ");
+    printf("Enter two numbers: example: 2.5 3.5\n");
     if (scanf("%lf %lf", &num1, &num2) != 2) {
         printf("Error: Invalid numbers.\n");
         return 1;
@@ -23,9 +26,9 @@ int main(){
     } else if (func == '/'){
         if (num2 != 0){
             printf("Result: %f\n", num1 / num2);
-            return 1;
         } else {
             printf("Error: Division by zero is not allowed.\n");
+            return 1;
         }
     }
     return 0;
